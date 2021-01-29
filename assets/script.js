6ed759d1253b26e284cab38efc2e84276075a3cf
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-01-29 17:33:41 -0500

    created index html with reset css and bootstrap

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
-
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=39.9526&lon=75.1652&appid=a72e07c808e0cdfc9d609b54001dafa3'
 console.log("hello");
+function getApi() {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=39.9526&lon=75.1652&appid=a72e07c808e0cdfc9d609b54001dafa3'
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+}
+
+console.log(requestUrl);
 })
